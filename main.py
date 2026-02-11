ab61d43d12c6ebe86b218ec36e9bf1f5597fa7c6
kayljiyan <kylegianlatina@gmail.com>
2026-02-11 08:47:58 +0800

    feat: upload files via api

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import boto3
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
 
-s3 = boto3.client('s3')
+app = FastAPI()
+s3 = boto3.client("s3")
 
-file = input("Enter file name: ")
-try:
-    s3.upload_file(file, "my-python-boto3-app", file)
-except Exception as e:
-    print(str(e))
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = file.file.read()
+    file.file.seek(0)
+    try:
+        s3.upload_fileobj(file.file, "my-python-boto3-app", file.filename)
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        file.file.close()
+    return {"message": "File has been uploaded"}

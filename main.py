87d22806ea7e3c801833b989ba5aa03650af8738
kayljiyan <kylegianlatina@gmail.com>
2026-02-18 15:17:01 +0800

    feat: add environment variables for security

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-import boto3
+import boto3, os
 from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 app = FastAPI()
-s3 = boto3.client("s3")
+s3 = boto3.client(
+    service_name="s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
 
 
 @app.post("/uploadfile/")
@@ -11,7 +22,7 @@ async def create_upload_file(file: UploadFile = File(...)):
     contents = file.file.read()
     file.file.seek(0)
     try:
-        s3.upload_fileobj(file.file, "my-python-boto3-app", file.filename)
+        s3.upload_fileobj(file.file, S3_BUCKET_NAME, file.filename)
     except Exception as e:
         return {"error": str(e)}
     finally:
